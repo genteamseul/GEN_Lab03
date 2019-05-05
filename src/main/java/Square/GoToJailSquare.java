f6453976d8ff5e6e5437f72799207a6348e5e8ea
unknown <batach31@hotmail.com>
2019-05-05 23:26:34 +0200

    final commit with comments and all tests required

--- src/main/java/Square/GoToJailSquare.java
+++ src/main/java/Square/GoToJailSquare.java
@@ -2,13 +2,18 @@ package Square;
 
 import Player.Player;
 
+/**
+ * Class representing the Go To Jail square on a monopoly board
+ */
 public class GoToJailSquare extends Square {
     public GoToJailSquare() {
         super("GO TO JAIL", 30);
     }
 
+
     @Override
     public void landedOn(Player player) {
+        //actually go to jail
         player.getPiece().setLocation(JailSquare.getSquareInstance());
     }
 }
--- src/main/java/Square/IncomeTaxSquare.java
+++ src/main/java/Square/IncomeTaxSquare.java
@@ -2,6 +2,9 @@ package Square;
 
 import Player.Player;
 
+/**
+ * Class representing the Tax square on a monopoly board
+ */
 public class IncomeTaxSquare extends Square {
     public IncomeTaxSquare() {
         super("Income Tax", 4);
@@ -9,6 +12,7 @@ public class IncomeTaxSquare extends Square {
 
     @Override
     public void landedOn(Player player) {
+        //youmust pay on this square
         player.payDay(Math.min(200, player.getNetWorth()*0.1));
     }
 }
