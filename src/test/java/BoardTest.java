9aa4ab958f9172bd310ff538c7cb915ba788f2fa
unknown <batach31@hotmail.com>
2019-05-05 23:09:46 +0200

    now we can test correctly

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,17 +1,34 @@
 import Game.Board;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
 
-    @Test
+    @BeforeEach
     void goodBoardCreation(){
         Board board = new Board();
         assertEquals("GO", board.start().toString());
         for(int i = 1; i < 40; i++){
-            //if()
+            assertEquals(board.getSquare(board.start(),i).getIndex(), i);
         }
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1","9"})
+    public void throwIfLessThanTwoPlayersOrMoreThanEight(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThrows(IllegalArgumentException.class, () -> new MonopolyGame());
+
+    }
+
 
 }
